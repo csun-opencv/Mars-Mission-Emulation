6f85dfd3892b47e526d32b011c0478768921317f
Hendy <abdullah.hendy.30@my.csun.edu>
2023-11-19 00:31:58 -0800

    first commit Mars Mission Emulation

---
+++ drone-pi-main.py
@@ -0,0 +1,288 @@
+# Import packages
+import os
+import argparse
+import cv2
+import numpy as np
+import sys
+import time
+import serial
+import keyboard
+from threading import Thread
+from tflite_runtime.interpreter import Interpreter
+from tflite_runtime.interpreter import load_delegate
+
+#################################################################
+# WHERE TO RUN THE CODE 
+#################################################################
+# Run the code from the same directory where the model folder exists
+
+#################################################################
+# IMPORTANT 
+#################################################################
+# 1. Camera
+    # This code will only work on non-Rasbpian OS (eg. Ubuntu 20.04), on Rasbpian, use picamera2
+    # Detect camera in Ubuntu 20.04:
+    # $ sudo usermod -aG video <username>
+    # $ sudo nano /boot/firmware/config.txt
+    # Add the following: start_x=1
+    # save and reboot
+# 2. Coral USB Accelerator
+    # pip install tflite-runtime==2.9.1  (2.9.1 is a MUST)
+    # $ sudo apt install libedgetpu1-std or sudo apt install libedgetpu1-max (ONLY 1 SHOULD BE THERE, DEPENDING ON THE DESIRED PERFORMANCE)
+    # Python 3.6-3.9 or less is required (TESTED 3.8 ONLY) 
+
+
+class VideoStream:
+    """Camera object that controls video streaming from the Picamera"""
+    def __init__(self,resolution=(640,480),framerate=30):
+        # Initialize the PiCamera and the camera image stream
+        self.stream = cv2.VideoCapture(0)
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(cv2.CAP_PROP_FRAME_WIDTH,resolution[0])
+        ret = self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT,resolution[1])
+        ret = self.stream.set(cv2.CAP_PROP_FPS, framerate)
+            
+        # Read first frame from the stream
+        (self.grabbed, self.frame) = self.stream.read()
+
+	# Variable to control when the camera is stopped
+        self.stopped = False
+
+    def start(self):
+	# Start the thread that reads frames from the video stream
+        Thread(target=self.update,args=()).start()
+        return self
+
+    def update(self):
+        # Keep looping indefinitely until the thread is stopped
+        while True:
+            # If the camera is stopped, stop the thread
+            if self.stopped:
+                # Close camera resources
+                self.stream.release()
+                return
+
+            # Otherwise, grab the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+	# Return the most recent frame
+        return self.frame
+
+    def stop(self):
+	# Indicate that the camera and thread should be stopped
+        self.stopped = True
+
+# Define window size
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+
+# Define and parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--coral', help='Use Coral Edge TPU Accelerator to speed up detection',
+                    action='store_true', default=False)
+parser.add_argument('--debug', help='Enable debugging eg. show camera window, draw boxes, show fps, etc',
+                    action='store_true', default=False)
+parser.add_argument('--pics', help='Enable taking pictures mode. Pics will get saved in recordings folder',
+                    action='store_true', default=False)
+args = parser.parse_args()
+
+min_conf_threshold = 0.35
+use_TPU = args.coral
+debug = args.debug
+pics = args.pics
+
+if debug:
+    print(f"Using Coral: {use_TPU}")
+    print(f"Debugging: {debug}")
+    print(f"Taking Pics: {pics}")
+
+# Get path to current working directory
+CWD_PATH = os.getcwd()      
+
+# Path to .tflite file, which contains the model that is used for object detection
+# If using Edge TPU, assign filename for Edge TPU model
+if use_TPU:
+    PATH_TO_GRAPH = os.path.join(CWD_PATH, 'models', 'custom_quant_edge_model_lite_new', 'edgetpu.tflite')
+else:
+    PATH_TO_GRAPH = os.path.join(CWD_PATH, 'models', 'custom_quant_edge_model_lite_new', 'detect_quant.tflite')
+
+# Path to label map file
+PATH_TO_LABELS = os.path.join(CWD_PATH, 'models', 'custom_quant_edge_model_lite', 'labelmap.txt')
+
+# Load the label map
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+
+# Load the Tensorflow Lite model.
+# If using Edge TPU, use special load_delegate argument
+if use_TPU:
+    interpreter = Interpreter(model_path=PATH_TO_GRAPH,
+                              experimental_delegates=[load_delegate('libedgetpu.so.1')])
+else:
+    interpreter = Interpreter(model_path=PATH_TO_GRAPH)
+
+interpreter.allocate_tensors()
+
+# Get model details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+
+# Check output layer name to determine if this model was created with TF2 or TF1,
+# because outputs are ordered differently for TF2 and TF1 models
+outname = output_details[0]['name']
+
+if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+    boxes_idx, classes_idx, scores_idx = 1, 3, 0
+
+# Initialize HC-05 Bluetooth module and bind r
+os.system('sudo rfcomm bind rfcomm0 00:14:03:05:01:C5')
+BTserialChannel = serial.Serial("/dev/rfcomm0", baudrate=115200)
+
+# Initialize video stream
+videostream = VideoStream(resolution=(width, height),framerate=30).start()
+
+time.sleep(1)
+if debug:
+    cv2.namedWindow('Object Detector', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Object Detector', WINDOW_WIDTH, WINDOW_HEIGHT)
+
+# Initialize detection flags to check every frame
+red_flag = False
+yellow_flag = False
+green_flag = False
+robot_flag = False
+
+if pics:
+    counter = 0
+    if not os.path.exists('recordings'):
+        print("NO /recordings folder. Creating one...")
+        os.makedirs('recordings/')
+    else:
+        print('/recordings folder exists. Cleaning it...')
+        os.system('rm -fR recordings/*')
+
+
+# For frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+while True:
+
+    # Start timer (for calculating frame rate) if debug
+    if debug or pics:
+        timer = cv2.getTickCount()
+
+    # Reset flags every frame
+    red_flag = False
+    yellow_flag = False
+    green_flag = False
+    robot_flag = False
+
+    frame1 = videostream.read()
+
+    # Acquire frame and resize to expected shape [1xHxWx3]
+    frame = frame1.copy()
+
+    # Acquire frame and resize to expected shape [1xHxWx3]
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    input_data = np.expand_dims(frame_rgb, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.invoke()
+
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+
+    # Loop over all detections and draw detection box if confidence is above minimum threshold
+    for i in range(len(scores)):
+
+        # If all elements in the array are below the threshold, skip
+        if all(score < min_conf_threshold for score in scores):
+            continue
+
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+            ymin = int(max(1,(boxes[i][0] * height)))
+            xmin = int(max(1,(boxes[i][1] * width)))
+            ymax = int(min(height,(boxes[i][2] * height)))
+            xmax = int(min(width,(boxes[i][3] * width)))
+
+            # Get detected class name
+            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+
+            # Update flags after analyzing an element in the scores list
+            if object_name == 'red_cone':
+                red_flag = True
+            elif object_name == 'yellow_cone':
+                yellow_flag = True
+            elif object_name == 'green_cone':
+                green_flag = True
+            elif object_name == 'robot':
+                robot_flag = True                    
+
+            if debug or pics:
+                cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+                # Draw label
+                label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+
+    # Send UART commands based on the flags each frame
+    if robot_flag and green_flag:
+        data = "g".encode()
+    elif robot_flag and yellow_flag:
+        data = "y".encode()
+    elif robot_flag and red_flag:
+        data = "r".encode()
+    else:
+        data = "p".encode()
+
+    try:
+        BTserialChannel.write(data)  
+    except:
+        # Close
+        os.system("echo 'ERORR: COULDNT SEND'")
+        cv2.destroyAllWindows()
+        videostream.stop()
+        exit()
+
+    if debug:
+        # Print data
+        print(f"sending: {data}")     
+
+    if debug or pics:
+        # Calculate framerate
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+        # Draw framerate in corner of frame
+        cv2.putText(frame, 'FPS: {0:.2f}'.format(fps),(30,50), cv2.FONT_HERSHEY_SIMPLEX,1, (255,255,0), 2, cv2.LINE_AA)
+
+        # If pics mode
+        if pics:
+            filename = f"recordings/pic_{counter}.jpg"
+            cv2.imwrite(filename, frame)
+            counter = counter + 1
+
+    if debug:
+        # Display results on the frame
+        cv2.imshow('Object Detector', frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
+            break
+    else:
+        time.sleep(0.001)
+        if keyboard.is_pressed('esc'):
+            break
+# Close
+cv2.destroyAllWindows()
+videostream.stop()
---
+++ ground_platform_ws/ground_platform_project/src/main.c
@@ -0,0 +1,393 @@
+
+#include <stdint.h>
+#include <math.h>
+#include "msp.h"
+#include "../inc/Clock.h"
+#include "../inc/CortexM.h"
+#include "../inc/GPIO.h"
+#include "../inc/EUSCI_A0_UART.h"
+#include "../inc/EUSCI_A2_UART.h"
+#include "../inc/Timer_A0_PWM.h"
+#include "../inc/Timer_A1_Interrupt.h"
+#include "../inc/Timer_A3_Capture.h"
+#include "../inc/Motor.h"
+#include "../inc/LPF.h"
+#include "../inc/Analog_Distance_Sensor.h"
+#include "../inc/SysTick_Interrupt.h"
+
+
+
+
+
+// Controller
+//#define DEBUG_ACTIVE    1
+
+// Initialize constant distance values (in mm)
+#define TOO_CLOSE_DISTANCE_GREEN  150
+#define TOO_CLOSE_DISTANCE_YELLOW 300
+#define DESIRED_DISTANCE    500
+
+// Initialize constant PWM duty cycle values for the motors
+#define PWM_NOMINAL         2500
+#define PWM_SWING           1000
+#define PWM_MIN             (PWM_NOMINAL - PWM_SWING)
+#define PWM_MAX             (PWM_NOMINAL + PWM_SWING)
+
+// Declare an enum named 'State'
+enum State {
+    IDLE,
+    GREEN,
+    RED,
+    YELLOW,
+    ERROR
+};
+
+// Use typedef to create a new type name 'state' for the 'enum State'
+typedef enum State state;
+
+// Declare a variable of type 'state'
+state  curr_state = IDLE;
+
+// Declare an enum named 'State'
+enum Yellow_State
+{
+    Y_STRAIGHT,
+    Y_AVOID
+};
+
+// Use typedef to create a new type name 'state' for the 'enum State'
+typedef enum Yellow_State yellow_state;
+
+// Declare a variable of type 'state'
+yellow_state yellow_curr_state = Y_STRAIGHT;
+
+enum Detection_State
+{
+    FOLLOW,
+    AVOID
+};
+
+uint16_t yellow_avoid_counter = 0;
+
+// Declare global variables used to store filtered distance values from the Analog Distance Sensor
+uint32_t Filtered_Distance_Left;
+uint32_t Filtered_Distance_Center;
+uint32_t Filtered_Distance_Right;
+
+// Declare global variables used to store converted distance values from the Analog Distance Sensor
+int32_t Converted_Distance_Left;
+int32_t Converted_Distance_Center;
+int32_t Converted_Distance_Right;
+
+// Declare global variable used to store the amount of error
+int32_t Error;
+
+// Proportional Controller Gain
+int32_t Kp = 4;
+
+// Initialize set point to 250 mm
+int32_t Set_Point = 250;
+
+// Declare global variables used to update PWM duty cycle values for the motors
+uint16_t Duty_Cycle_Left;
+uint16_t Duty_Cycle_Right;
+
+// Command Flags
+int complete_flag = 0;
+
+void FSM(void);
+void Stop_WatchDog(void);
+void Action(uint8_t flag);
+void Green_Task();
+void Yellow_Task();
+void Red_Task();
+void Listening(uint8_t received_data);
+void Sample_Analog_Distance_Sensor();
+void Timer_A1_Periodic_Task(void);
+
+
+
+int main(void)
+{
+    // Declare variables for Analog Distance Sensor
+    uint32_t Raw_A17;
+    uint32_t Raw_A14;
+    uint32_t Raw_A16;
+
+    // Ensure that interrupts are disabled during initialization
+    DisableInterrupts();
+
+    // Stop watch dog
+    Stop_WatchDog();
+
+    // Initialize the 48 MHz Clock
+    Clock_Init48MHz();
+    // Initialize the built-in red LED
+    LED1_Init();
+
+    // Initialize the front and back LEDs
+    P8_Init();
+
+    // Init RGB
+    RGB_Init();
+
+    // Init all UARTs
+    EUSCI_A0_UART_Init();
+    EUSCI_A2_UART_Init();
+
+    // Initialize EUSCI_A0_UART
+    EUSCI_A0_UART_Init_Printf();
+
+    // Initialize Timer A1 periodic interrupt with a rate of 2 kHz
+    Timer_A1_Interrupt_Init(&Timer_A1_Periodic_Task, TIMER_A1_INT_CCR0_VALUE);
+
+    // Init Motor
+    Motor_Init();
+
+    // Initialize motor duty cycle values
+    Duty_Cycle_Left  = PWM_NOMINAL;
+    Duty_Cycle_Right = PWM_NOMINAL;
+
+    // Initialize the Analog Distance Sensor using ADC14
+    Analog_Distance_Sensor_Init();
+
+    // Start conversion of Analog Distance Sensor raw values
+    Analog_Distance_Sensor_Start_Conversion(&Raw_A17, &Raw_A14, &Raw_A16);
+
+    // Initialize low-pass filters for the Analog Distance Sensor
+    LPF_Init(Raw_A17, 64);
+    LPF_Init2(Raw_A14, 64);
+    LPF_Init3(Raw_A16, 64);
+
+    // SysTic Init
+    SysTick_Interrupt_Init(SYSTICK_INT_NUM_CLK_CYCLES, SYSTICK_INT_PRIORITY);
+
+    // Enable the interrupts used by Timer A1 and other modules
+    EnableInterrupts();
+
+    while(1){
+        FSM();
+//        Listening(EUSCI_A2_UART_InChar());
+//        printf("%c\n", EUSCI_A2_UART_InChar());
+#ifdef DEBUG_ACTIVE
+        printf("state: %d\r\n", curr_state);
+        printf("yellow state: %d\r\n", yellow_curr_state);
+        printf("Left: %d mm | Center: %d mm | Right: %d mm\n", Converted_Distance_Left, Converted_Distance_Center, Converted_Distance_Right);
+        Clock_Delay1us(500);
+#endif
+    }
+}
+
+void FSM(void) {
+    switch (curr_state) {
+        case IDLE:
+            yellow_curr_state = Y_STRAIGHT;
+            complete_flag = 0;
+            yellow_avoid_counter = 0;
+            RGB_Output(RGB_LED_WHITE);
+            Motor_Stop();
+//            Listening(EUSCI_A2_UART_InChar());
+            Listening('y');
+            break;
+
+        case GREEN:
+            if (!complete_flag) {
+                RGB_Output(RGB_LED_GREEN);
+                Green_Task();
+            } else {
+                curr_state = IDLE;
+            }
+            break;
+
+        case YELLOW:
+            if (!complete_flag) {
+                RGB_Output(RGB_LED_YELLOW);
+                Yellow_Task();
+            } else {
+                curr_state = IDLE;
+            }
+            break;
+
+        case RED:
+            if (!complete_flag) {
+                RGB_Output(RGB_LED_RED);
+                Red_Task();
+            } else {
+                curr_state = IDLE;
+            }
+            break;
+
+        default:
+            RGB_Output(RGB_LED_PINK);
+            Motor_Stop();
+            break;
+    }
+}
+
+void Stop_WatchDog(void){
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
+}
+
+void Listening(uint8_t received_data){
+    if (received_data == 'g') {
+        curr_state = GREEN;
+
+    } else if  (received_data == 'y'){
+        curr_state = YELLOW;
+
+    } else if  (received_data == 'r'){
+        curr_state = RED;
+
+    } else if  (received_data == 'p'){
+        curr_state = IDLE;
+        Motor_Stop();
+
+    } else {
+        curr_state = IDLE;
+    }
+}
+
+// Go straight and detect cone (object follower-ish)
+void Green_Task()
+{
+    if ((Converted_Distance_Left < TOO_CLOSE_DISTANCE_GREEN) || (Converted_Distance_Center < TOO_CLOSE_DISTANCE_GREEN) || (Converted_Distance_Right < TOO_CLOSE_DISTANCE_GREEN)) {
+        complete_flag = 1;
+    } else {
+        Action(FOLLOW);
+    }
+}
+
+// Drive and avoid
+void Yellow_Task() {
+    if (yellow_curr_state == Y_STRAIGHT)
+    {
+        if ((Converted_Distance_Left < TOO_CLOSE_DISTANCE_YELLOW) || (Converted_Distance_Center < TOO_CLOSE_DISTANCE_YELLOW) || (Converted_Distance_Right < TOO_CLOSE_DISTANCE_YELLOW))
+        {
+            yellow_curr_state = Y_AVOID;
+            yellow_avoid_counter = 0;
+        }
+        else
+        {
+            Action(FOLLOW);
+        }
+    } else if (yellow_curr_state == Y_AVOID) {
+        Action(AVOID);
+    }
+}
+
+// Rotate in place
+void Red_Task() {
+
+    // Apply the updated PWM duty cycle values to the motors
+#ifndef DEBUG_ACTIVE
+    Motor_Forward(5000, 3000);
+#endif
+    Clock_Delay1ms(5000);
+#ifndef DEBUG_ACTIVE
+    Motor_Forward(3000, 5000);
+#endif
+    Clock_Delay1ms(5000);
+    complete_flag = 1;
+}
+
+void Action(uint8_t flag) {
+
+    // Check if both the left and right distance sensor readings are greater than the desired distance
+    if ((Converted_Distance_Left > DESIRED_DISTANCE) && (Converted_Distance_Right > DESIRED_DISTANCE))
+    {
+        // Calculate the set point as the average of the left and right sensor distance readings
+        Set_Point = (Converted_Distance_Left + Converted_Distance_Right) / 2;
+    }
+    else
+    {
+        // If at least one distance sensor reading is below the desired distance, assign the set point to the desired distance
+        Set_Point = DESIRED_DISTANCE;
+    }
+
+    // Calculate the error based on the sensor readings
+    if (Converted_Distance_Left < Converted_Distance_Right)
+    {
+        Error = Converted_Distance_Left - Set_Point;
+    }
+    else
+    {
+        Error = Set_Point - Converted_Distance_Right;
+    }
+
+    if (flag == FOLLOW) {
+        // Calculate the new duty cycle for the right motor based on the error and proportional constant (Kp)
+        Duty_Cycle_Right = PWM_NOMINAL - (Kp * Error);
+
+        // Calculate the new duty cycle for the left motor based on the error and proportional constant (Kp)
+        Duty_Cycle_Left = PWM_NOMINAL + (Kp * Error);
+
+    } else if (flag == AVOID){
+        // Calculate the new duty cycle for the right motor based on the error and proportional constant (Kp)
+        Duty_Cycle_Right = PWM_NOMINAL + (Kp * Error);
+
+        // Calculate the new duty cycle for the left motor based on the error and proportional constant (Kp)
+        Duty_Cycle_Left = PWM_NOMINAL - (Kp * Error);
+    }
+
+    // Ensure that the duty cycle for the right motor does not go below the minimum PWM value
+    if (Duty_Cycle_Right < PWM_MIN)
+        Duty_Cycle_Right = PWM_MIN;
+
+    // Ensure that the duty cycle for the right motor does not exceed the maximum PWM value
+    if (Duty_Cycle_Right > PWM_MAX)
+        Duty_Cycle_Right = PWM_MAX;
+
+    // Ensure that the duty cycle for the left motor does not go below the minimum PWM value
+    if (Duty_Cycle_Left < PWM_MIN)
+        Duty_Cycle_Left = PWM_MIN;
+
+    // Ensure that the duty cycle for the left motor does not exceed the maximum PWM value
+    if (Duty_Cycle_Left > PWM_MAX)
+        Duty_Cycle_Left = PWM_MAX;
+
+#ifndef DEBUG_ACTIVE
+    // Apply the updated PWM duty cycle values to the motors
+
+    Motor_Forward(Duty_Cycle_Left, Duty_Cycle_Right);
+#endif
+}
+
+void SysTick_Handler(void)
+{
+    if (yellow_curr_state == Y_AVOID) {
+        if (yellow_avoid_counter < 2000) {
+            yellow_avoid_counter++;
+        } else {
+            complete_flag = 1;
+            yellow_avoid_counter = 0;
+        }
+    }
+}
+
+
+void Sample_Analog_Distance_Sensor()
+{
+    // Declare variables for Analog Distance Sensor
+    uint32_t Raw_A17;
+    uint32_t Raw_A14;
+    uint32_t Raw_A16;
+
+    // Start conversion of Analog Distance Sensor raw values
+    Analog_Distance_Sensor_Start_Conversion(&Raw_A17, &Raw_A14, &Raw_A16);
+
+    // Apply low-pass filter to raw values
+    Filtered_Distance_Right = LPF_Calc(Raw_A17);
+    Filtered_Distance_Center = LPF_Calc2(Raw_A14);
+    Filtered_Distance_Left = LPF_Calc3(Raw_A16);
+
+    // Convert filtered distance values using calibration formula
+    Converted_Distance_Left = Analog_Distance_Sensor_Calibrate(Filtered_Distance_Left);
+    Converted_Distance_Center = Analog_Distance_Sensor_Calibrate(Filtered_Distance_Center);
+    Converted_Distance_Right = Analog_Distance_Sensor_Calibrate(Filtered_Distance_Right);
+}
+
+
+void Timer_A1_Periodic_Task(void)
+{
+    Sample_Analog_Distance_Sensor();
+}
